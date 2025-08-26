b60abfe4d8d6c0546caec511d3211974fc647823
Tentenjnr <nlediemmanuel1010@gmail.com>
2025-08-26 18:55:06 +0100

    Tentenjnr

--- src/UI/Service/MainService.jsx
+++ src/UI/Service/MainService.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Service1 from './Service1'
+import Service2 from './Service2'
 
 function MainService() {
   return (
-    <div>MainService</div>
+    <div>
+      <Service1/>
+      <Service2/>
+    </div>
   )
 }
 
---
+++ src/UI/Service/Service1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Service1() {
+  return (
+    <div>
+        <div className='h-[400px] w-full bg-[url(Serverpic1.jpg)] servicepic'>
+            <h1 className='text-white text-7xl font-bold pt-32 text-center'>Our Services</h1>
+            <p className='text-white text-center pt-4'>Home / Services</p>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Service1
